5c976bc0f38cdfd0c8a2eeafee0b4ef144ee383c
denismahmutovic <djdeny95@gmail.com>
2022-09-21 19:08:26 +0200

    Pocetak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1 className="text-4xl font-bold bg-red-100">Hello world!</h1>
+      </div>
+
+      <div className="flex justify-between flex-row items-center w-1/2 mx-auto h-24 bg-orange-500 mt-20 rounded-xl px-8">
+        <div className="flex justify-center items-center bg-blue-500 w-16 h-16 text-white font-bold underline rounded-lg">
+          1
+        </div>
+        <div className="flex justify-center items-center bg-red-500 w-16 h-16 text-white font-bold underline rounded-full">
+          2
+        </div>
+        <div className="flex justify-center items-center bg-green-500 w-16 h-16 text-white font-bold underline rounded-2xl">
+          3
+        </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+// karktica slika  naslov cena
