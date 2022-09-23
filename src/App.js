c6ea87a0e1b1dff133be057870a77ec7adf8e542
denismahmutovic <djdeny95@gmail.com>
2022-09-23 19:36:08 +0200

    loading

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
-import React from "react";
-import Card from "./Components/Card/Card";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/Navbar/Navbar";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import CartPage from "./pages/CartPage/CartPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <>
-      {/* <div className="App">
-        <h1 className="text-4xl font-bold bg-red-100">Hello world!</h1>
-      </div>
+    <div>
+      <NavBar />
 
-      <div className="flex justify-between flex-row items-center w-1/2 mx-auto h-24 bg-orange-500 mt-20 rounded-xl px-8">
-        <div className="flex justify-center items-center bg-blue-500 w-16 h-16 text-white font-bold underline rounded-lg">
-          1
-        </div>
-        <div className="flex justify-center items-center bg-red-500 w-16 h-16 text-white font-bold underline rounded-full">
-          2
-        </div>
-        <div className="flex justify-center items-center bg-green-500 w-16 h-16 text-white font-bold underline rounded-2xl">
-          3
-        </div>
-      </div> */}
-      <div className="flex justify-center items-center">
-        <Card
-          price="30"
-          text="Replay muška majica kratkih rukava. Okrugli izrez. Pamuk. Teget boja."
-          images={
-            "https://www.n-sport.net/UserFiles/products/big/09/12/muska-majica-replay-316522832P-098.jpg"
-          }
-          title={"Replay Maica"}
-        />
-        <Card
-          price="40"
-          text="Replay muška majica kratkih rukava. Okrugli izrez. Pamuk. Teget boja."
-          images={
-            "https://img.ep-cdn.com/images/500/500/kg/kgkupgqfiytzjsnldglr.jpg"
-          }
-          title={"Replay Maica"}
-        />
-      </div>
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </div>
   );
 }
 
 export default App;
-
-// karktica slika  naslov cena
--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Toaster } from "react-hot-toast";
 
 import { BrowserRouter } from "react-router-dom";
+import { ShoppingCartContextProvider } from "./context/ShoppingCartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ShoppingCartContextProvider>
+        <Toaster position="bottom-right" />
+        <App />
+      </ShoppingCartContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
